c658fa06379bbac6b27de29486a5df0687cb4a58
Andrea Zonca <code@andreazonca.com>
2023-12-30 23:30:19 +0100

    feat: Order in API

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,6 @@ from api.views import *
 
 
 urlpatterns = [
-
-	re_path("product/((?P<pk>\d+)/)?", csrf_exempt(ProductView.as_view())),
-
+    re_path("product/((?P<pk>\d+)/)?", csrf_exempt(ProductView.as_view())),
+    re_path("order/((?P<pk>\d+)/)?", csrf_exempt(OrderView.as_view())),
 ]
