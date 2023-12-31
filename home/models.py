2f7f8533ec3becbbf894d2ba538e4b75bb4f8ba7
Andrea Zonca <code@andreazonca.com>
2023-12-31 19:13:39 +0100

    feat: use unique uuid as primary key

--- home/models.py
+++ home/models.py
@@ -1,19 +1,16 @@
 from django.db import models
-
-# Create your models here.
-
-
-class Product(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    info = models.CharField(max_length=100, default="")
-    price = models.IntegerField(blank=True, null=True)
-
-    def __str__(self):
-        return self.name
+from shortuuid.django_fields import ShortUUIDField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Order(models.Model):
+    id = ShortUUIDField(
+        length=8,
+        prefix="id_",
+        alphabet="abcdefghijkmnpqrstuvwxyz23456789",
+        primary_key=True,
+    )
     index = models.BigIntegerField(blank=True, null=True)
     SaleOrdId = models.BigIntegerField(blank=True, null=True)
     InternalOrdNo = models.TextField(blank=True, null=True)
